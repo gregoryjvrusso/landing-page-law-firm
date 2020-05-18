b1c2df468da4135038951f1a5b2ce9b8fbcf3618
Gregory Russo <gregory.russo@luizalabs.com>
2020-05-17 21:48:48 -0300

    fix: update app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import { GlobalStyles } from "./common/globalStyles";
 import { ThemeProvider } from "styled-components";
 
+import Header from "./components/modules/Header"
+import Featured from "./components/modules/Featured"
+import Purpose from "./components/modules/Purpose"
+import AboutUs from "./components/modules/AboutUs"
+
 import theme from "./common/theme";
 
 const App = () => {
@@ -9,6 +14,10 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <React.Fragment>
         <GlobalStyles />
+        <Header />
+        <Featured />
+        <Purpose />
+        <AboutUs />
       </React.Fragment>
     </ThemeProvider>
   );
